b57af6b5e35ceb2777b564ccf24004a55f5a5fe0
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-12-04 18:14:35 -0800

    setting username and password for dev

--- server/sql-db/index.js
+++ server/sql-db/index.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "pwd",
+  user: "curmudgeon",
+  password: "password",
   database:"digitaldining"
 });
 
