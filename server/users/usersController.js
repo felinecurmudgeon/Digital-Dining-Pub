f3efea68c1433a9813c7ce84bdba1532b3537417
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-12-07 17:19:25 -0800

    updated API routes to model

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -1,20 +1,31 @@
+var usersModel = require('./usersModel.js');
+
 module.exports = {
-  getAllUsers : function (req, res) {
-    console.log('getting users');
-    res.status(200);
-    res.send('stub success');
-  },
+  getAllUsers : function (req, res){
+    console.log("getting users");
+    usersModel.user.get()
+      .then(function(users) {
+        res.status(200);
+        res.send(users);
+      });
+  }, 
 
-  getUser : function (req, res) {
-    console.log('getting user ', req.params.id);
-    res.status(200);
-    res.send('stub success');
-  },
+  getUser : function (req, res){
+    console.log("getting user ", req.params.id);
+    usersModel.user.get(req.params.id)
+      .then(function(users) {
+        res.status(200);
+        res.send(users);
+      });
+  }, 
 
-  createUser : function (req, res) {
-    console.log('creating user ');
-    res.status(201);
-    res.send('stub success');
+  createUser : function (req, res){
+    console.log("creating user ", req.body);
+    usersModel.user.post(req.body)
+      .then(function (data){
+        res.status(201);
+        res.send(data);
+      });
   },
 
   updateUser : function (req, res) {
