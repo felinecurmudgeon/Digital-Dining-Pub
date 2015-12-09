1db5a13fe636898ae8c97717bae27d38af4a2533
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-12-08 19:06:47 -0800

    modified query to work...

--- server/orders/ordersModel.js
+++ server/orders/ordersModel.js
@@ -14,7 +14,7 @@ module.exports = {
         var menu_items = parameters.menu_items.map(function(el) {
           return [parameters.party_id, el.menu_item_id, el.quantity, ordered_at];
         });
-        db.con.query('INSERT into menu_items_ordered (party_id, menu_item_id, quantity, ordered_at) set ?', menu_items, function (err, data) {
+        db.con.query('INSERT INTO menu_items_ordered (party_id, menu_item_id, quantity, ordered_at) VALUES ?', menu_items, function (err, data) {
           if(err){
             reject(err);
           } else {
