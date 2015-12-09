0abc16552b58e695d5709e8c5f672ed79baeb324
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-12-08 20:00:26 -0800

    added commented code for testing (to be passed in spec)

--- server/parties/partiesController.js
+++ server/parties/partiesController.js
@@ -3,6 +3,40 @@
 var partiesModel = require('./partiesModel.js');
 var url = require('url');
 
+// var redis = require('redis');
+// var client = redis.createClient();
+// client.quit();
+// var redis = {};
+
+
+// var usersModel = require('../users/usersModel.js');
+
+// usersModel.user.post({
+//         username: 'setTime',
+//         password: '11123'})
+//   .then(function(data) {
+//     console.log(data.insertId);
+//     console.log(data);
+//     usersModel.user.put({
+//       id: data.insertId,
+//       password: 'caca'
+//     });
+//   })
+//   .then(function() {
+//     partiesModel.party.checkInAndCreateParty({
+//       restaurant_id: 1,
+//       party_size: 5,
+//       user_id: 1});
+//   })
+//   .then(function(data) {
+//     partiesModel.party.seatParty({
+//       party_id: data.insertId,
+//       table_id: 1});
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+
 module.exports = {
   checkInAndCreateParty: function (req, res) {
     partiesModel.party.checkInAndCreateParty(req.body)
