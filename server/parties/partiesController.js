e2679c982de1cb412e7ce576b47e9eabd6eb1dd3
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-12-08 19:00:37 -0800

    removed commented code

--- server/parties/partiesController.js
+++ server/parties/partiesController.js
@@ -2,40 +2,6 @@
 
 var partiesModel = require('./partiesModel.js');
 
-// var redis = require('redis');
-// var client = redis.createClient();
-// client.quit();
-// var redis = {};
-
-
-// var usersModel = require('../users/usersModel.js');
-
-// usersModel.user.post({
-//         username: 'setTime',
-//         password: '11123'})
-//   .then(function(data) {
-//     console.log(data.insertId);
-//     console.log(data);
-//     usersModel.user.put({
-//       id: data.insertId,
-//       password: 'caca'
-//     });
-//   })
-//   .then(function() {
-//     partiesModel.party.checkInAndCreateParty({
-//       restaurant_id: 1,
-//       party_size: 5,
-//       user_id: 1});
-//   })
-//   .then(function(data) {
-//     partiesModel.party.seatParty({
-//       party_id: data.insertId,
-//       table_id: 1});
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
-
 module.exports = {
   checkInAndCreateParty: function (req, res) {
     partiesModel.party.checkInAndCreateParty(req.body)
@@ -63,12 +29,3 @@ module.exports = {
     }
   }
 };
-
-
-/*
-
-available tables will be stored in a redis set availableTable:restauId
-menus will be stored in 
-
-
-*/
