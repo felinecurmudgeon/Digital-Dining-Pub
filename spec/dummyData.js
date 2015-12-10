1b0e46981e8477a58467df9c72f4735698b5dd65
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-12-09 17:45:52 -0800

    create tables and users too

--- spec/dummyData.js
+++ spec/dummyData.js
@@ -2,9 +2,9 @@
 var db = require('../server/sql-db/index.js');
 var Promise = require('bluebird');
 
-var deleteData = function (table, filter_column, filter_value) {
+var deleteWholeDataFromTable = function (table) {
   return new Promise( function (resolve, reject) {
-    db.con.query('DELETE FROM ?? WHERE ?? = ?', [table, filter_column, filter_value], function (err, data) {
+    db.con.query('DELETE FROM ??', table, function (err, data) {
       if (err) {
         reject(err);
       } else {
@@ -78,97 +78,135 @@ var insertOrUpdateUser = function (table, user) { //only for unique key constrai
 };
 
 var restaurantUsers = [
-    {username: 'toto',
-    password: 'pwd'},
-    
-    {username: 'emilio',
-    password: 'qwerty'},
-    
-    {username: 'ZinedineZidane',
-    password: 'france98'},
-    
-    {username: 'tobias',
-    password: '123456'}
-  ];
+  {username: 'toto',
+  password: 'pwd'},
+  
+  {username: 'emilio',
+  password: 'qwerty'},
+  
+  {username: 'ZinedineZidane',
+  password: 'france98'},
+  
+  {username: 'tobias',
+  password: '123456'}
+];
+
+var users = [
+  {username: 'toto',
+  password: 'pwd'},
+  
+  {username: 'emilio',
+  password: 'qwerty'},
+  
+  {username: 'ZinedineZidane',
+  password: 'france98'},
+  
+  {username: 'tobias',
+  password: '123456'}
+];
 
 var restaurants = [
-    {restaurant_name: 'Pizza Pino',
-    restaurant_address: '944 Market Street',
-    restaurant_city: 'San Francisco',
-    restaurant_state: 'California',
-    restaurant_zip_code: 94102},
+  {restaurant_name: 'Pizza Pino',
+  restaurant_address: '944 Market Street',
+  restaurant_city: 'San Francisco',
+  restaurant_state: 'California',
+  restaurant_zip_code: 94102},
 
-    {restaurant_name: 'Happy Burgers',
-    restaurant_address: '234 Mission Street',
-    restaurant_city: 'San Francisco',
-    restaurant_state: 'California',
-    restaurant_zip_code: 94105},
-    
-    {restaurant_name: 'Chez Denise',
-    restaurant_address: '9 avenue de l\'indépendance américaine',
-    restaurant_city: 'San Francisco',
-    restaurant_state: 'California',
-    restaurant_zip_code: 94122,
-    opening_hour_monday:'11:00:00',
-    closing_hour_monday: '22:30:00',
-    opening_hour_tuesday:'11:00:00',
-    closing_hour_tuesday: '22:30:00',
-    opening_hour_wednesday: '00:00:00',
-    closing_hour_wednesday: '00:00:00',
-    opening_hour_thursday: '11:00:00',
-    closing_hour_thursday: '22:30:00',
-    opening_hour_friday: '11:00:00',
-    closing_hour_friday: '25:00:00',
-    opening_hour_saturday: '11:00:00',
-    closing_hour_saturday: '25:00:00',
-    opening_hour_sunday: '00:00:00',
-    closing_hour_sunday: '00:00:00'},
-    
-    {restaurant_name: 'Lolinda',
-    restaurant_address: '2518 Mission St',
-    restaurant_city: 'San Francisco',
-    restaurant_state: 'California',
-    restaurant_zip_code: 94110}
-  ];
+  {restaurant_name: 'Happy Burgers',
+  restaurant_address: '234 Mission Street',
+  restaurant_city: 'San Francisco',
+  restaurant_state: 'California',
+  restaurant_zip_code: 94105},
+  
+  {restaurant_name: 'Chez Denise',
+  restaurant_address: '9 avenue de l\'indépendance américaine',
+  restaurant_city: 'San Francisco',
+  restaurant_state: 'California',
+  restaurant_zip_code: 94122,
+  opening_hour_monday:'11:00:00',
+  closing_hour_monday: '22:30:00',
+  opening_hour_tuesday:'11:00:00',
+  closing_hour_tuesday: '22:30:00',
+  opening_hour_wednesday: '00:00:00',
+  closing_hour_wednesday: '00:00:00',
+  opening_hour_thursday: '11:00:00',
+  closing_hour_thursday: '22:30:00',
+  opening_hour_friday: '11:00:00',
+  closing_hour_friday: '25:00:00',
+  opening_hour_saturday: '11:00:00',
+  closing_hour_saturday: '25:00:00',
+  opening_hour_sunday: '00:00:00',
+  closing_hour_sunday: '00:00:00'},
+  
+  {restaurant_name: 'Lolinda',
+  restaurant_address: '2518 Mission St',
+  restaurant_city: 'San Francisco',
+  restaurant_state: 'California',
+  restaurant_zip_code: 94110}
+];
+
+var tables = [
+  [{table_number: 1, seats: 4},
+  {table_number: 2, seats: 4},
+  {table_number: 3, seats: 4},
+  {table_number: 4, seats: 4}],
+
+  [{table_number: 1, seats: 4},
+  {table_number: 2, seats: 6},
+  {table_number: 3, seats: 4},
+  {table_number: 4, seats: 4},
+  {table_number: 5, seats: 6},
+  {table_number: 6, seats: 6}],
+
+  [{table_number: 1, seats: 4},
+  {table_number: 2, seats: 4},
+  {table_number: 3, seats: 4},
+  {table_number: 4, seats: 4}],
+
+  [{table_number: 1, seats: 2},
+  {table_number: 2, seats: 4},
+  {table_number: 3, seats: 4},
+  {table_number: 4, seats: 2}]
+];
 
 var menuCategories = [ //each subArray will be affected to the corresponding restaurant above
-    [{category_name: 'Appetizer'},
-    {category_name: 'Drinks'}],
-    
-    [{category_name: 'Soups'},
-    {category_name: 'Deserts'}],
-    
-    [{category_name: 'Appetizer'},
-    {category_name: 'Drinks'}],
-    
-    [{category_name: 'Main Courses'},
-    {category_name: 'Deserts'}]
-  ];
+  [{category_name: 'Appetizer'},
+  {category_name: 'Drinks'}],
+  
+  [{category_name: 'Soups'},
+  {category_name: 'Deserts'}],
+  
+  [{category_name: 'Appetizer'},
+  {category_name: 'Drinks'}],
+  
+  [{category_name: 'Main Courses'},
+  {category_name: 'Deserts'}]
+];
 
 var menuItems = [ /*each subArray will be affected to the corresponding restaurant above
-                  and each nested subArray inside a subArray will be affected to the corresponding menu_category above*/
-    [[{title: 'Prosciutto', 
-    description: 'ham and bread with small sliced tomatoes', 
-    price: 5.95}], 
-    [{title: 'Birra Moretti', 
-    description: 'Lager beer', 
-    price: 7.65}]],
-    
-    [[],
-    [{title: 'Cheesecake', 
-    description: 'low fat and organic', 
-    price: 6}]],
+                and each nested subArray inside a subArray will be affected to the corresponding menu_category above*/
+  [[{title: 'Prosciutto', 
+  description: 'ham and bread with small sliced tomatoes', 
+  price: 5.95}], 
+  [{title: 'Birra Moretti', 
+  description: 'Lager beer', 
+  price: 7.65}]],
+  
+  [[],
+  [{title: 'Cheesecake', 
+  description: 'low fat and organic', 
+  price: 6}]],
 
-    [[{title: 'Onion Soup', 
-    description: '100% home-made (onions grow in our backyard)', 
-    price: 8.85}],
-    []],
-    
-    [[{title: 'Angus Steak', 
-    description: 'from Wisconsin', 
-    price: 12}],
-    []]
-  ];
+  [[{title: 'Onion Soup', 
+  description: '100% home-made (onions grow in our backyard)', 
+  price: 8.85}],
+  []],
+  
+  [[{title: 'Angus Steak', 
+  description: 'from Wisconsin', 
+  price: 12}],
+  []]
+];
 
 module.exports = {
   run: function (){
@@ -183,8 +221,14 @@ module.exports = {
           return insertData('restaurants', restaurants[index]);
         })
         .then(function (restaurantCreated) {
+          return Promise.all(tables[index].map(function (table) {
+            table.restaurant_id = restaurantCreated.id;
+            return insertData('tables', table);
+          }));
+        })
+        .then(function (tablesCreated) {
           return Promise.all(menuCategories[index].map(function (menuCategory) {
-            menuCategory.restaurant_id = restaurantCreated.id;
+            menuCategory.restaurant_id = tablesCreated[0].restaurant_id;
             return insertData('menu_categories', menuCategory);
           }));
         })
@@ -200,6 +244,10 @@ module.exports = {
         .then(function (data) {
           console.log('restaurant created: ', data);
         });
+    });
+
+    users.forEach(function (user, index) {
+      insertOrUpdateUser('users', user)
     });
   }
 };
