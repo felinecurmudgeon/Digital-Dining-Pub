6a7ff4e28db1e162ce6dd92ec1b113240238b204
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-12-11 14:56:21 -0800

    removed empty and repopulate db from server file

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('./middleware/logger');
 var expressJwt = require('express-jwt');
-var dummy = require('../spec/dummyData.js');
 
 var app = express();
 var expressRouter = express.Router();
@@ -40,8 +39,6 @@ app.use('/', expressRouter);
 router(expressRouter);
 
 app.listen(8000);
-
-dummy.emptyAndRepopulateDB();
 
 module.exports = app;
 
