25e7944b9efeb9b15cad50ddab0b65ab4f85b4b1
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-12-11 10:51:28 -0800

    removed commented code

--- server/parties/partiesController.js
+++ server/parties/partiesController.js
@@ -22,39 +22,6 @@ var createJsonResponseForParty = function (data) {
   }
   return JsonResponseObject;
 };
-// var redis = require('redis');
-// var client = redis.createClient();
-// client.quit();
-// var redis = {};
-
-
-// var usersModel = require('../users/usersModel.js');
-
-// usersModel.user.post({
-//         username: 'setTime',
-//         password: '11123'})
-//   .then(function(data) {
-//     console.log(data.insertId);
-//     console.log(data);
-//     usersModel.user.put({
-//       id: data.insertId,
-//       password: 'caca'
-//     });
-//   })
-//   .then(function() {
-//     partiesModel.party.checkInAndCreateParty({
-//       restaurant_id: 1,
-//       party_size: 5,
-//       user_id: 1});
-//   })
-//   .then(function(data) {
-//     partiesModel.party.seatParty({
-//       party_id: data.insertId,
-//       table_id: 1});
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
 
 module.exports = {
   checkInAndCreateParty: function (req, res) {
