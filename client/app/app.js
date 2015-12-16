07d6bdd660fbd9d5215ef8febf8992d0ee486518
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-12-15 19:11:52 -0800

    routing works, and first functionnal version of authentication

--- client/app/app.js
+++ client/app/app.js
@@ -1,27 +1,49 @@
-angular.module('digitalDining', ['ui.router'])
+angular.module('digitalDining', [
+  'ui.router',
+  'digitalDining.services',
+  'digitalDining.auth'])
 
-.config(function($stateProvider, $urlRouterProvider) {
-    
-    $urlRouterProvider.otherwise('/home');
-    
-    $stateProvider
-        
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('kitchen', {
-            url: '/kitchen',
-            templateUrl: 'kitchen.html' // TODO
-        })
-        .state('reserations', {
-            url: '/reserations',
-            templateUrl: 'reserations.html' // TODO
-        })
-        .state('menuCreator', {
-            url: '/menuCreator',
-            templateUrl: 'menuCreator.html' // TODO
-        })
-        .state('restaurantSettings', {
-            url: '/restaurantSettings',
-            templateUrl: 'restaurantSettings.html' // TODO
-        });
-        
+.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/reserations');
+  $stateProvider
+    .state('login', {
+      url:'/login',
+      templateUrl: './app/auth/login.html',
+      controller: 'AuthController'
+    })
+    .state('signup', {
+      url:'/signup',
+      templateUrl: './app/auth/signup.html',
+      controller: 'AuthController'
+    })
+    .state('kitchen', {
+      url: '/kitchen',
+      templateUrl: './app/dummy.html' // TODO add controller
+    })
+    .state('reserations', {
+      url: '/reserations',
+      templateUrl: './app/dummy.html' // TODO
+    })
+    .state('menuCreator', {
+      url: '/menuCreator',
+      templateUrl: './app/dummy.html' // TODO
+    })
+    .state('restaurantSettings', {
+      url: '/restaurantSettings',
+      templateUrl: './app/dummy.html' // TODO
+    });
+})
+
+.run(function($rootScope, $location, $state, Auth) {
+    $rootScope.$on('$stateChangeStart', function (e, toState  , toParams, fromState, fromParams) {
+        var isLogin = (toState.name === 'login' || toState.name === 'signup');
+        if(isLogin){
+           return; // no need to redirect 
+        }
+        var userInfo = Auth.isAuth();
+        if(userInfo === false) {
+            e.preventDefault(); // stop current execution
+            $state.go('login'); // go to login
+        }
+    });
 });
