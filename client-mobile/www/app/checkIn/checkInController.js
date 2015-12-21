299ebafcc7413bd67df7a0bee15b56e993ed0992
Adam Grimm <ATGrimm@gmail.com>
2015-12-20 16:12:02 -0800

    Displayed default messages on certain views when you are not checked in

--- client-mobile/www/app/checkIn/checkInController.js
+++ client-mobile/www/app/checkIn/checkInController.js
@@ -3,18 +3,22 @@ angular.module('dd-checkInCtrl', [])
 
 .controller('CheckInCtrl', ['$scope', '$state', '$window', 'HomeFactory', 'CheckInFactory', function ($scope, $state, $window, HomeFactory, CheckInFactory) {
   $scope.isCheckedIn = false;
-  console.log('routed to checkIn');
+  $scope.hasRestaurant = true;
+  $scope.focusedRestaurant = null;
+  $scope.partyInfo = {};
 
-  $scope.updateCheckedInStatus = function () {
+  $scope.updateStatus = function () {
     if ($window.localStorage.getItem('partyInfo')) {
       $scope.isCheckedIn = true;
     } else {
       $scope.isCheckedIn = false;
     }
-    console.log('$scope.isCheckedIn is ', $scope.isCheckedIn);
+    if (Object.keys($scope.focusedRestaurant).length > 0) {
+      $scope.hasRestaurant = true;
+    } else {
+      $scope.hasRestaurant = false;
+    }
   };
-
-  $scope.updateCheckedInStatus();
 
   $scope.getCheckInStatus = function () {
     return $scope.isCheckedIn;
@@ -22,20 +26,19 @@ angular.module('dd-checkInCtrl', [])
 
   $scope.getFocusedRestaurant = function () {
     $scope.focusedRestaurant = HomeFactory.getFocusedRestaurant();
+    $scope.partyInfo.restaurant_id = $scope.focusedRestaurant.id;
+    $scope.partyInfo.party_size = '';
+    console.log($scope.focusedRestaurant);
   };
-  $scope.getFocusedRestaurant();
 
-  $scope.partyInfo = {
-    restaurant_id: $scope.focusedRestaurant.id,
-    party_size: ''
-  };
   $scope.doCheckIn = function () {
     CheckInFactory.doCheckIn($scope.partyInfo).then( function () {
       $state.go('nav.restaurantMenu');
+      $scope.isCheckedIn = true;
     });
-    // setTimeout(function () {
-    //   $state.go('nav.restaurantMenu')
-    // },2000);
-    $scope.isCheckedIn = true;
   };
+
+  $scope.getFocusedRestaurant();
+  $scope.updateStatus();
+
 }]);
