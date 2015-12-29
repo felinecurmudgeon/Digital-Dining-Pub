dd690b9d80ff9f85e7f8880fbb99589a8d1a0422
Adam Grimm <ATGrimm@gmail.com>
2015-12-29 09:48:05 -0800

    tuff

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,12 +2,15 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/myApp
 echo $PATH
+echo 'hi mom'
 # npm install
-sudo env "PATH=$PATH" bower install --allow-root
+# env "PATH=$PATH" bower install
+echo 'hi dad'
 # forever stopall
 # sudo service mysqld start
-# forever start server/server.js
-cd client-mobile
-# npm install
-sudo env "PATH=$PATH" bower install --allow-root
-ionic serve -a
+# # forever start server/server.js
+# cd client-mobile
+# # npm install
+# sudo env "PATH=$PATH" bower install --allow-root
+# ionic serve -a
+
